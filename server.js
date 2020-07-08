0bf7973e11dfa33df0816069cf156444f020a10f
pilichanampe <pilichanampe@hotmail.com>
2020-07-08 18:41:05 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server); 
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Run when a client connects
+io.on('connection', socket => {
+  console.log('New WS Connection...')
+});
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
